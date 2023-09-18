18cda3cfe3a9dd217dee6422278c4b09f871209d
mgourou <mohamedgourou@outlook.fr>
2023-09-18 22:35:15 +0200

    navbar/footer/section1

--- projet/app/views.py
+++ projet/app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     return render(request, 'app/home.html')
+
+def products(request):
+    return render(request, 'app/products.html')
