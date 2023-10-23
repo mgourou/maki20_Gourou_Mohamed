658858969d6dc31d094ec1320c11cd4a148c2884
mgourou <mohamedgourou@outlook.fr>
2023-10-23 14:36:13 +0200

    vercel

--- projet/projet/settings.py
+++ projet/projet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x+t2x!13v0g+ja^uqq%5yqi)t++gb$a8k^a4asap$4bggxcz64
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', "127.0.0.1"]
 
 
 # Application definition
